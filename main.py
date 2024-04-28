d252ada48b681ea1058a6f60f28b7de7f535cedf
Brennan Elliott <bcje2567@gmail.com>
2024-04-28 12:02:10 -0700

    car can rotate but not good

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ running = True
 dt=0
 class Car(pygame.sprite.Sprite):
 
-    def __init__(self, health, speed, damage, width, height):
+    def __init__(self, health, speed, damage, width, height, angle):
         self.health=health
         self.speed=speed
         self.damage=damage
         self.height=height
         self.width=width
-
+        self.angle=angle
         self.image = pygame.image.load("./Insporation/camoTruckNOBG.png")
 
         self.centerX = screen.get_width() // 2
         self.centerY = screen.get_height() // 2
 
     def re_draw(self):
-        self.rect = pygame.Rect(self.centerX, self.centerY, self.width, self.height)
-        screen.blit(self.image, self.rect)
+        
+        print(self.angle)   
+        # self.rect = pygame.Rect(self.centerX, self.centerY, self.width, self.height)
+
+        screen.blit(self.image, self.image.get_rect())
 
 truck_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-truck = Car(100, 20, 0, 100, 100)
+truck = Car(100, 20, 0, 100, 100, 0)
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -39,14 +42,15 @@ while running:
     #pygame.draw.rect(screen, "blue",(100,100,100,100))
     # RENDER YOUR GAME HERE
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and truck.centerY >= 0:
         truck.centerY -= 300 * dt
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and truck.centerY <= 550:
         truck.centerY += 300 * dt
-    if keys[pygame.K_a]:
-        truck.centerX -= 300 * dt
-    if keys[pygame.K_d]:
-        truck.centerX += 300 * dt
+    if keys[pygame.K_a]: #and truck.centerX >= 0:
+        truck.angle -= 1 * dt
+        truck.image = pygame.transform.rotate(truck.image, 1) 
+    # if keys[pygame.K_d]: #and truck.centerX <= 1180:
+    #     truck.angle += 15
 
     # flip() the display to put your work on screen
     truck.re_draw()
